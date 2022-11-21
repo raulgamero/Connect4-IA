d7031d778007acec940c21f17a53d05e176ee090
Raul Gamero <raulgamero03@gmail.com>
2022-11-21 17:53:38 +0100

    added poda

--- src/edu/epsevg/prop/lab/c4/C4Panther.java
+++ src/edu/epsevg/prop/lab/c4/C4Panther.java
@@ -16,20 +16,13 @@ public class C4Panther
     
     private int prof, cont_jugades = 0, color;
     public int[][] taulaHeurisitca = null;
-    private Timer timer;
+    private boolean poda = true;
+    private int millorHeuristica;
     
     public C4Panther(int profunditat)
     {
         nom = "C4Panther";
         prof = profunditat;
-        timer = new Timer(5500, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("TIME OUT");
-                //variable_Booleana_De_La_Clase = true; //en el minimax poner un semaforo de que si esto esta activo devuelva la mejor que tenga actualmente
-            }
-        });
-
     }
     
     public String nom()
@@ -46,19 +39,15 @@ public class C4Panther
     @Override
     public int moviment(Tauler t, int color)
     {
-        // empezamos el contador para que no pase de 1 minuto
-        //timer.start();
-
         // creamos tabla dinamica para la heuristica y hacemos print
         if (taulaHeurisitca == null) {
             this.color = color;
             creaTaulaHeurisitca(t);
         }
-
-        int columna = miniMax(t, prof);
-        System.out.println("Jugades explorades = "+ cont_jugades);
         
-        //timer.stop();
+        millorHeuristica = 0;
+        int columna = miniMax(t, prof);
+        System.out.println("Jugades explorades = "+ cont_jugades + ". Millor heuristica = " + millorHeuristica);
 
         return columna;
     }
@@ -71,12 +60,16 @@ public class C4Panther
 
         for (int i = 0; i < t.getMida(); i++) {
             if (t.movpossible(i)) {
-                if (mejor_mov == -1) mejor_mov = i;
                 Tauler t_aux = new Tauler(t);
                 t_aux.afegeix(i, color);
                 int valorNou = min(t_aux, i, profu-1, alpha, beta);
+                if (mejor_mov == -1){
+                    millorHeuristica = valorNou;
+                    mejor_mov = i;
+                }
                 if(valorNou > max){
                     max = valorNou;
+                    millorHeuristica = max;
                     mejor_mov = i;
                 }
             }
@@ -87,7 +80,10 @@ public class C4Panther
 
     private int min(Tauler t_aux, int columna, int profunditat, int alpha, int beta) {
         
-        if (t_aux.solucio(columna, color)) return 1000000;
+        if (t_aux.solucio(columna, color)) {
+            cont_jugades = cont_jugades + 1;
+            return 1000000;
+        }
         else if (profunditat == 0 || !(t_aux.espotmoure())) return heu(t_aux, columna);
         
         int min_value = 10000;
@@ -99,7 +95,7 @@ public class C4Panther
                 min_value = Math.min(max(t_min, i, profunditat-1, alpha, beta), min_value);
                 beta = Math.min(beta, min_value);
                 // poda alpha-beta
-                if (alpha >= beta) break;
+                if (poda && alpha >= beta) break;
             }
         }
         
@@ -108,7 +104,10 @@ public class C4Panther
 
     private int max(Tauler t_aux, int columna, int profunditat, int alpha, int beta) {
         
-        if (t_aux.solucio(columna, color*-1)) return -1000000;
+        if (t_aux.solucio(columna, color*-1)) {
+            cont_jugades = cont_jugades + 1;
+            return -1000000;
+        }
         else if (profunditat == 0 || !(t_aux.espotmoure())) return heu(t_aux, columna);
         
         int max_value = -10000;
@@ -120,7 +119,7 @@ public class C4Panther
                 max_value = Math.max(min(t_max, i, profunditat-1, alpha, beta), max_value);
                 alpha = Math.max(alpha, max_value);
                 // poda alpha-beta
-                if (alpha >= beta) break;
+                if (poda && alpha >= beta) break;
             }
         }
     
@@ -146,34 +145,6 @@ public class C4Panther
         
     
     private void creaTaulaHeurisitca (Tauler t) {
-        /*taulaHeurisitca = new int [][] {
-        {3, 4, 5, 7, 7, 5, 4, 3},
-        {4, 6, 8,10,10, 8, 6, 4},
-        {5, 8,11,13,13,11, 8, 5},
-        {7,10,13,16,16,13,10, 7},
-        {7,10,13,16,16,13,10, 7},
-        {5, 8,11,13,13,11, 8, 5},
-        {4, 6, 8,10,10, 8, 6, 4},
-        {3, 4, 5, 7, 7, 5, 4, 3}
-        };*/
-        /*taulaHeurisitca = new int[t.getMida()][t.getMida()];
-        for (int i = 0; i < t.getMida(); i++) {
-            for (int j = 0; j < t.getMida(); j++) {
-                if(i < t.getMida()/2){
-                    if(j < t.getMida()/2){
-                        taulaHeurisitca[i][j] = (i*2)+(j*2)+1;
-                    }else{
-                        taulaHeurisitca[i][j] = (i*2)+((t.getMida()-j-1)*2)+1;
-                    }
-                }else{
-                    if(j < t.getMida()/2){
-                        taulaHeurisitca[i][j] = ((t.getMida()-i-1)*2)+(j*2)+1;
-                    }else{
-                        taulaHeurisitca[i][j] = ((t.getMida()-i-1)*2)+((t.getMida()-j-1)*2)+1;
-                    }
-                }
-            }
-        }*/
         taulaHeurisitca = new int[t.getMida()][t.getMida()];
         int ini = 2;
         for (int i = 0; i < t.getMida(); i++) {
@@ -191,13 +162,6 @@ public class C4Panther
             if(i < t.getMida()/2) ini +=1;
             else ini -= i-1;
         }
-        for (int i = 0; i < t.getMida(); i++) {
-            for (int j = 0; j < t.getMida(); j++) {
-                System.out.print(taulaHeurisitca[i][j] + " ");
-            }
-            System.out.println("");
-        }
-
     }
         
 }
